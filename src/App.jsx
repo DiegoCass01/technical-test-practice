23e919dfd2c277a07e4a80cf88921f53fed8d154
DiegoCass01 <diego.castillo170901@gmail.com>
2024-11-22 00:01:41 -0600

    getting and showing the image

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import { useEffect, useState } from 'react'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/says/'
 
 export function App () {
   const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
+      .then(data => {
+        // getting the fact
+        const { fact } = data
+        setFact(fact)
+        // getting first word
+        const firstWord = fact.split(' ')[0]
+        // and setting the url with the fact's first word
+        setImageUrl(`${CAT_PREFIX_IMAGE_URL}${firstWord}`)
+      })
   }, [])
 
   return (
     <main>
       <h1>App de Gatitos</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Extracted from ${fact}`} height={500} />}
     </main>
   )
 }
