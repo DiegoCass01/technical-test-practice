8e04db00c9cf00cf542c075753682638e1e04f26
DiegoCass01 <diego.castillo170901@gmail.com>
2024-11-21 19:42:02 -0600

    showing a random fact

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-export function App() {
-    return (
-        <h1>App de Gatitos</h1>
-    )
+import { useEffect, useState } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
+export function App () {
+  const [fact, setFact] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, [])
+
+  return (
+    <main>
+      <h1>App de Gatitos</h1>
+      {fact && <p>{fact}</p>}
+    </main>
+  )
 }
