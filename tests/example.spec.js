531acd43c8629ae46d3fde4a033f80782a31fc04
DiegoCass01 <diego.castillo170901@gmail.com>
2024-11-26 01:08:37 -0600

    testing -update paragraph when click

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -9,11 +9,16 @@ test('app shows random fact and image', async ({ page }) => {
 
   const text = page.getByRole('paragraph')
   const image = page.getByRole('img')
+  const button = page.getByRole('button')
 
   const textContent = await text.textContent()
   const imageSrc = await image.getAttribute('src')
 
-  console.log({ textContent, imageSrc })
+  await button.click()
+
+  const textContentUpdated = await text.textContent()
+
   await expect(textContent?.length).toBeGreaterThan(0)
   await expect(imageSrc?.startsWith(CAT_PREFIX_IMAGE_URL)).toBeTruthy()
+  await expect(textContent).not.toBe(textContentUpdated)
 })
